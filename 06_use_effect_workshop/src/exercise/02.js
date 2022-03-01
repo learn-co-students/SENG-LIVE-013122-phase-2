251d3925af654f2f93cb0c58309467375f00d356
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-03-01 14:46:33 -0800

    add solution to useEffect workshop

--- 06_use_effect_workshop/src/exercise/02.js
+++ 06_use_effect_workshop/src/exercise/02.js
@@ -2,15 +2,21 @@ import { useState, useEffect } from "react";
 
 function App() {
   // ✅ update the initial state for text to read a value using localStorage.getItem("text")
-  const [text, setText] = useState("");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState(localStorage.getItem("text1") || "");
+  const [count, setCount] = useState(localStorage.getItem("counter1") || 0);
 
   useEffect(() => {
     // ✅ when the side effect runs, use localStorage.setItem("text", value) to save the text to localStorage
     console.log("Running side effect");
-  });
+    localStorage.setItem("text1", text)
+  }, [text]);
   // ✅ use the dependencies array so that the side effect only runs if the text changes
 
+  useEffect(() => {
+    // ✅ when the side effect runs, use localStorage.setItem("text", value) to save the text to localStorage
+    console.log("Running side effect");
+    localStorage.setItem("counter1", count)
+  }, [count]);
   console.log("Rendering component");
 
   return (
